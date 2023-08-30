00c7207d6cdf25022f8c6ba398f4253e2da6072d
MURILO BARBOZA DE SOUZA <murilo.bsouza3@senacsp.edu.br>
2023-08-29 22:08:07 -0300

    entrada de dados adicionada

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void something_new() {
+    printf("Entre com um valor inteiro: ");
+    scanf("%d", &num);
+    printf("%d", num)
+
+}
+
 int main() {
-    printf("Hello World!!");
+    printf("Hello World!! \n");
+    something_new();
     return 0;
 }
